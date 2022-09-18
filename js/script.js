f4c14b413d9ae64e231464de372216d7c4e6b6f9
lingacode <lingarajpatra480@gmail.com>
2022-09-18 18:15:59 +0000

    add some data

--- js/script.js
+++ js/script.js
@@ -20,3 +20,21 @@ function sliceForMe(stringvalue,start,end){
 function concat(firstName,lastName){
 	document.getElementById('concat').innerHTML=firstName.concat(" ",lastName);
 }
+function toUpperCase(string){
+	document.getElementById('uppercase').innerHTML=string.toUpperCase();
+}
+function toLowerCase(string){
+	document.getElementById('lowercase').innerHTML=string.toLowerCase();
+}
+function trim(string){
+	document.getElementById('trim').innerHTML=string.trim();
+}
+function replace( string,replacement,change){
+     document.getElementById('replace').innerHTML=string.replace(replacement,change);
+}
+function  indexof(string,paragraph){
+	document.getElementById('indexof').innerHTML=string.indexOf(paragraph);
+}
+function  lastindexof(string,graph){
+	document.getElementById('indexof').innerHTML=string.lastIndexOf(graph);
+}
