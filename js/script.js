18e21f952f7afdd33dccb8cd45618710d50fdf7a
lingacode <lingarajpatra480@gmail.com>
2022-09-23 08:44:30 +0000

    add

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function printMyName(args){
 
 }
 function getMyLength(stringvalue){
-	document.getElementById('num').innerHTML=stringvalue.length;
+	document.getElementById('number').innerHTML=stringvalue.length;
 }
 function sliceForMe(stringvalue,start,end){
 	document.getElementById('sliceForMe').innerHTML=stringvalue.slice(start,end);
@@ -36,18 +36,20 @@ function  indexof(string,paragraph){
 	document.getElementById('indexof').innerHTML=string.indexOf(paragraph);
 }
 function  lastindexof(string,graph){
-	document.getElementById('indexof').innerHTML=string.lastIndexOf(graph);
+	document.getElementById('lastindexof').innerHTML=string.lastIndexOf(graph);
 }
 function  include(string,graph){
-	document.getElementById('indexof').innerHTML=string.includes(graph);	
+	document.getElementById('boolean').innerHTML=string.includes(graph);	
 }
+function  thestring(string){
+	document.getElementById('stringss').innerHTML=string.toString();	
+}
+
 function match(string,value){
 	var text="/"+value+"/g";
 	console.log(text);
 	document.getElementById('out').innerHTML=string.match(text);
 }
-
-
 
 function printOption(num){
 	var htmlOutPut="Index: <select>";
